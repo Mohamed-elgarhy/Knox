5b0962844db3c1dc3aaf775549763673e52a1736
machd <machd@macs-mbp.fritz.box>
2019-03-01 15:17:10 +0100

    refactor

--- java/Konux.assignment/src/main/java/com/konux/interview/assignment/service/LogServiceImpl.java
+++ java/Konux.assignment/src/main/java/com/konux/interview/assignment/service/LogServiceImpl.java
@@ -15,8 +15,6 @@ public class LogServiceImpl implements LogService {
 
 	@Override
 	public void logServiceDetails(LogEntry logEntry) {
-		System.out.println(logEntry);
-		
 		LogEntryProto.LogEntry entry =  LogEntryProto.LogEntry.newBuilder()
 		.setTimestamp(logEntry.getTimestamp())
 		.setUserId(logEntry.getUserId())
@@ -27,16 +25,11 @@ public class LogServiceImpl implements LogService {
 		Socket server;
 		try {
 			server = new Socket("localhost",7234);
-			System.out.println("Connected");
 			OutputStream out = server.getOutputStream();
 			entry.writeTo(out);
-			//out.write(logMessageAsByteArray);
-
-			System.out.println("Sent as bytes : "+logMessageAsByteArray);
 			out.close();
 			server.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
